8f4962005e8eef3111a97e8386587e60550545f2
M33 <327-m33@git.qoto.org>
2021-04-02 11:27:19 +0000

    Update Sierpiński-Triangle-v3.cr, Sierpiński-Triangle-v4.cr files

--- Sierpiński-Triangle-v3.cr
+++ Sierpiński-Triangle-v3.cr
@@ -1,1 +1,6 @@
-Sierpiński-Triangl
+y="   ▲   "
+u="  ▲ ▲  "
+i=" ▲   ▲ "
+o="▲ ▲ ▲ ▲"
+b="    "
+puts(b+b+b+y,b+b+b+u,b+b+b+i,b+b+b+o,b+b+y+" "+y,b+b+u+" "+u,b+b+i+" "+i,b+b+o+" "+o,b+y+b+b+" "+y,b+u+b+b+" "+u,b+i+b+b+" "+i,b+o+b+b+" "+o,y+" "+y+" "+y+" "+y,u+" "+u+" "+u+" "+u,i+" "+i+" "+i+" "+i,o+" "+o+" "+o+" "+o)
